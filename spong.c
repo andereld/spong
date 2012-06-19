8ff6ceb16068fb657086b75ed3b676ed77250675
Anders Wold Eldhuset <andereld@samfundet.no>
2012-06-19 04:28:35 +0200

    Set up SDL initialization and main event loop
    Also aligned some comments and added the 'spong' executable to
    .gitignore.

--- spong.c
+++ spong.c
@@ -2,5 +2,52 @@
 
 int main()
 {
+	SDL_Init(SDL_INIT_EVERYTHING);
+	TTF_Init();
+
+	/* the frame buffer */
+	SDL_Surface *screen = SDL_SetVideoMode(W_WIDTH, W_HEIGHT, 32,
+			SDL_SWSURFACE);
+
+	/* the background color of the screen */
+	Uint32 clearColor = SDL_MapRGB(screen->format, 0xFF, 0xFF, 0xFF);
+	SDL_FillRect(screen, &screen->clip_rect, clearColor);
+
+	Uint32 startTime;	/* used to note the beginning of each
+				   iteration of the main event loop */
+	bool running = true;	/* true till the application should exit */
+	while (running) {
+		startTime = SDL_GetTicks();
+
+		SDL_Event event;
+		while (SDL_PollEvent(&event)) {
+			switch (event.type) {
+			case SDL_QUIT:
+				running = false;
+				break;
+			case SDL_KEYDOWN:
+				switch (event.key.keysym.sym) {
+				case SDLK_q:
+					running = false;
+					break;
+				}
+				break;
+			}
+
+		}
+
+		/* render frame to screen */
+		SDL_Flip(screen);
+
+		/* keep a steady frame rate */
+		if (SDL_GetTicks()-startTime < FRAME_DURATION)
+			SDL_Delay(FRAME_DURATION - (SDL_GetTicks()-startTime));
+
+	}
+
+	TTF_Quit();
+
+	SDL_Quit();
+
 	return EXIT_SUCCESS;
 }
--- spong.h
+++ spong.h
@@ -8,28 +8,34 @@
 #include <stdbool.h>
 #include <stdlib.h>
 
+#define W_WIDTH 800			/* the width of the main window */
+#define W_HEIGHT 480			/* the height of the main window */
+
+#define FPS 30				/* frames per second */
+#define FRAME_DURATION ((int) 1000/FPS)	/* the duration of a frame in ms */
+
 typedef struct Court {
-	Uint16 x, y; /* position of the upper left corner */
-	Uint16 w, h; /* width and height */
-	SDL_Rect upperWall, lowerWall; /* the walls of the court */
+	Uint16 x, y;			/* position of the upper left corner */
+	Uint16 w, h;			/* width and height */
+	SDL_Rect upperWall, lowerWall;	/* the walls of the court */
 } Court;
 
 typedef struct Ball {
-	Sint8 vx, vy; /* the horizontal and vertical velocity of the ball */
-	SDL_Rect rect; /* the size and position of the ball */
-	Uint32 color; /* the color of the ball */
+	Sint8 vx, vy;	/* the horizontal and vertical velocity of the ball */
+	SDL_Rect rect;	/* the size and position of the ball */
+	Uint32 color;	/* the color of the ball */
 } Ball;
 
 typedef struct Racket {
-	Sint8 vy; /* the vertical velocity of the racket; a racket does not
-		     move horizontally */
-	SDL_Rect rect; /* the size and position of the racket */
-	Uint32 color; /* the color of the racket */
+	Sint8 vy;	/* the vertical velocity of the racket;
+			   a racket does not move horizontally */
+	SDL_Rect rect;	/* the size and position of the racket */
+	Uint32 color;	/* the color of the racket */
 } Racket;
 
 typedef struct Player {
-	Racket racket; /* a player controls a Racket ... */
-	Uint8 points; /* ... and has a number of points */
+	Racket racket;	/* a player controls a Racket ... */
+	Uint8 points;	/* ... and has a number of points */
 } Player;
 
 #endif
