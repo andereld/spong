341bdb4c86197b4f97df9b8d6435cd5bc87c03c7
Anders Wold Eldhuset <andereld@samfundet.no>
2012-06-24 06:57:06 +0200

    Added the ball
    Added logic for drawing and moving the ball, including collision
    detection and points. Added a 'prevY' field to the Paddle struct to keep
    track of whether a paddle is moving and, if so, in what direction.
    
    Also renamed player1 and player2 to the more descriptive lPlayer and
    rPlayer, for left and right respectively, and did some other minor
    fixes.

--- spong.c
+++ spong.c
@@ -20,10 +20,14 @@ int main(int argc, char *argv[])
 	if (!court) die();
 
 	/* set up the players and their paddles */
-	Player *player1 = makePlayer(screen);
-	Player *player2 = makePlayer(screen);
-	if (!player1 || !player2) die();
-	player2->paddle.rect.x = C_WIDTH - P_WIDTH;
+	Player *lPlayer = makePlayer(screen);
+	Player *rPlayer = makePlayer(screen);
+	if (!lPlayer || !rPlayer) die();
+	rPlayer->paddle.rect.x = C_X + C_WIDTH - P_WIDTH;
+
+	/* add the ball */
+	Ball *ball = makeBall(screen);
+	if (!ball) die();
 
 	/* set up the player controls; these need to be #defines to satisfy
 	   the compiler's idea of an 'integer constant' in a switch statement */
@@ -34,8 +38,8 @@ int main(int argc, char *argv[])
 	/* because SDL_KEY(UP|DOWN) only occur once, not continuously while
 	   the key is pressed, we need to keep track of whether a key is
 	   (still) pressed */
-	bool player1ShouldMoveUp = false, player1ShouldMoveDown = false,
-	     player2ShouldMoveUp = false, player2ShouldMoveDown = false;
+	bool lPlayerShouldMoveUp = false, lPlayerShouldMoveDown = false,
+	     rPlayerShouldMoveUp = false, rPlayerShouldMoveDown = false;
 
 	Uint32 startTime;	/* denotes the beginning of each iteration
 				   of the main event loop */
@@ -43,30 +47,46 @@ int main(int argc, char *argv[])
 	while (running) {
 		startTime = SDL_GetTicks();
 
-		/* overwrite the previous frame's paddles */
-		SDL_FillRect(screen, &player1->paddle.rect, clearColor);
-		SDL_FillRect(screen, &player2->paddle.rect, clearColor);
+		/* clear the previous frame's paddles and ball */
+		SDL_FillRect(screen, &lPlayer->paddle.rect, clearColor);
+		SDL_FillRect(screen, &rPlayer->paddle.rect, clearColor);
+		SDL_FillRect(screen, &ball->rect, clearColor);
+
+		/* redraw the walls in case they were clipped by the ball
+		   in a previous frame */
+		SDL_FillRect(screen, &court->upperWall, court->color);
+		SDL_FillRect(screen, &court->lowerWall, court->color);
 
 		/* get the current state of the players' controls */
 		readPlayerInput(&running,
-				&player1ShouldMoveUp, &player1ShouldMoveDown,
-				&player2ShouldMoveUp, &player2ShouldMoveDown);
+				&lPlayerShouldMoveUp, &lPlayerShouldMoveDown,
+				&rPlayerShouldMoveUp, &rPlayerShouldMoveDown);
+
+		/* save the current position of the paddles */
+		lPlayer->paddle.prevY = lPlayer->paddle.rect.y;
+		rPlayer->paddle.prevY = rPlayer->paddle.rect.y;
 
 		/* move the paddles if appropriate */
-		if (player1ShouldMoveUp)
-			movePaddle(court, &player1->paddle, UP);
-		else if (player1ShouldMoveDown)
-			movePaddle(court, &player1->paddle, DOWN);
-		if (player2ShouldMoveUp)
-			movePaddle(court, &player2->paddle, UP);
-		else if (player2ShouldMoveDown)
-			movePaddle(court, &player2->paddle, DOWN);
+		if (lPlayerShouldMoveUp)
+			movePaddle(court, &lPlayer->paddle, UP);
+		else if (lPlayerShouldMoveDown)
+			movePaddle(court, &lPlayer->paddle, DOWN);
+		if (rPlayerShouldMoveUp)
+			movePaddle(court, &rPlayer->paddle, UP);
+		else if (rPlayerShouldMoveDown)
+			movePaddle(court, &rPlayer->paddle, DOWN);
+
+		/* move the ball */
+		moveBall(court, ball, lPlayer, rPlayer);
 
 		/* draw the paddles */
-		SDL_FillRect(screen, &player1->paddle.rect,
-				player1->paddle.color);
-		SDL_FillRect(screen, &player2->paddle.rect,
-				player2->paddle.color);
+		SDL_FillRect(screen, &lPlayer->paddle.rect,
+				lPlayer->paddle.color);
+		SDL_FillRect(screen, &rPlayer->paddle.rect,
+				rPlayer->paddle.color);
+
+		/* draw the ball */
+		SDL_FillRect(screen, &ball->rect, ball->color);
 
 		/* render frame to screen */
 		SDL_Flip(screen);
@@ -77,10 +97,13 @@ int main(int argc, char *argv[])
 			SDL_Delay(FRAME_DURATION - elapsedTime);
 	}
 
+	printf("L: %d, R: %d\n", lPlayer->points, rPlayer->points);
+
 	/* free resources */
 	free(court);
-	free(player1);
-	free(player2);
+	free(lPlayer);
+	free(rPlayer);
+	free(ball);
 
 	TTF_Quit();
 	SDL_Quit();
@@ -100,18 +123,18 @@ Court *makeCourt(SDL_Surface *screen)
 
 	Court *court = malloc(sizeof(Court));
 
-	court->x = 0;
-	court->y = 0;
+	court->x = C_X;
+	court->y = C_Y;
 	court->w = C_WIDTH;
 	court->h = C_HEIGHT;
 
-	court->upperWall.x = 0;
-	court->upperWall.y = 0;
+	court->upperWall.x = C_X;
+	court->upperWall.y = C_Y;
 	court->upperWall.w = C_WIDTH;
 	court->upperWall.h = wt;
 
-	court->lowerWall.x = 0;
-	court->lowerWall.y = C_HEIGHT - wt;
+	court->lowerWall.x = C_X;
+	court->lowerWall.y = C_Y + C_HEIGHT - wt;
 	court->lowerWall.w = C_WIDTH;
 	court->lowerWall.h = wt;
 
@@ -127,9 +150,10 @@ Player *makePlayer(SDL_Surface *screen)
 {
 	Player *player = malloc(sizeof(Player));
 
-	/* make the leftmost player (x = 0) by default */
-	player->paddle.rect.x = 0;
-	player->paddle.rect.y = C_HEIGHT/2 - P_HEIGHT/2;
+	/* make the leftmost player (x = C_X) by default */
+	player->paddle.rect.x = C_X;
+	player->paddle.rect.y = (C_Y+C_HEIGHT)/2 - P_HEIGHT/2;
+	player->paddle.prevY = player->paddle.rect.y;
 	player->paddle.rect.w = P_WIDTH;
 	player->paddle.rect.h = P_HEIGHT;
 	player->paddle.color = SDL_MapRGB(screen->format, 0, 0, 0);
@@ -138,9 +162,25 @@ Player *makePlayer(SDL_Surface *screen)
 	return player;
 }
 
+Ball *makeBall(SDL_Surface *screen)
+{
+	Ball *ball = malloc(sizeof(Ball));
+
+	ball->rect.x = (C_X+C_WIDTH)/2 - B_SIZE/2;
+	ball->rect.y = (C_Y+C_HEIGHT)/2 - B_SIZE/2;
+	ball->rect.w = B_SIZE;
+	ball->rect.h = B_SIZE;
+	ball->color = SDL_MapRGB(screen->format, 0, 0, 0);
+
+	ball->vx = B_VX;
+	ball->vy = 0;
+
+	return ball;
+}
+
 void readPlayerInput(bool *running,
-		bool *player1ShouldMoveUp, bool *player1ShouldMoveDown,
-		bool *player2ShouldMoveUp, bool *player2ShouldMoveDown)
+		bool *lPlayerShouldMoveUp, bool *lPlayerShouldMoveDown,
+		bool *rPlayerShouldMoveUp, bool *rPlayerShouldMoveDown)
 {
 	SDL_Event event;
 	while (SDL_PollEvent(&event)) {
@@ -155,32 +195,32 @@ void readPlayerInput(bool *running,
 				break;
 
 			case P1_UP_KEY:
-				*player1ShouldMoveUp = true;
+				*lPlayerShouldMoveUp = true;
 				break;
 			case P1_DOWN_KEY:
-				*player1ShouldMoveDown = true;
+				*lPlayerShouldMoveDown = true;
 				break;
 			case P2_UP_KEY:
-				*player2ShouldMoveUp = true;
+				*rPlayerShouldMoveUp = true;
 				break;
 			case P2_DOWN_KEY:
-				*player2ShouldMoveDown = true;
+				*rPlayerShouldMoveDown = true;
 				break;
 			}
 			break;
 		case SDL_KEYUP:
 			switch (event.key.keysym.sym) {
 			case P1_UP_KEY:
-				*player1ShouldMoveUp = false;
+				*lPlayerShouldMoveUp = false;
 				break;
 			case P1_DOWN_KEY:
-				*player1ShouldMoveDown = false;
+				*lPlayerShouldMoveDown = false;
 				break;
 			case P2_UP_KEY:
-				*player2ShouldMoveUp = false;
+				*rPlayerShouldMoveUp = false;
 				break;
 			case P2_DOWN_KEY:
-				*player2ShouldMoveDown = false;
+				*rPlayerShouldMoveDown = false;
 				break;
 			}
 			break;
@@ -204,6 +244,69 @@ void movePaddle(Court *court, Paddle *paddle, Direction direction)
 		else
 			paddle->rect.y = lowerBound - paddle->rect.h;
 	}
+}
+
+void moveBall(Court *court, Ball *ball, Player *lPlayer, Player *rPlayer)
+{
+	Paddle *lPaddle = &lPlayer->paddle;
+	Paddle *rPaddle = &rPlayer->paddle;
+
+	/* set the velocity of the ball */
+	if (ball->rect.x <= lPaddle->rect.x+lPaddle->rect.w && ball->vx < 0) {
+		/* the ball is on the left boundary of the court
+		   and traveling leftwards */
+		Uint32 upperBound = lPaddle->rect.y;
+		Uint32 lowerBound = lPaddle->rect.y + lPaddle->rect.h;
+		if (ball->rect.y+ball->rect.h >= upperBound
+				&& ball->rect.y <= lowerBound) {
+			ball->vx = -ball->vx;
+			if (lPaddle->prevY != lPaddle->rect.y)
+				ball->vy += lPaddle->prevY - lPaddle->rect.y;
+		} else {
+			/* the right player scored a point */
+			rPlayer->points++;
+			resetAndServe(ball, LEFT);
+		}
+	} else if (ball->rect.x+ball->rect.w >= rPaddle->rect.x
+			&& ball->vx > 0) {
+		/* the ball is on the right boundary of the court
+		   and traveling rightwards */
+		Uint32 upperBound = rPaddle->rect.y;
+		Uint32 lowerBound = rPaddle->rect.y + rPaddle->rect.h;
+		if (ball->rect.y+ball->rect.h >= upperBound
+				&& ball->rect.y <= lowerBound) {
+			ball->vx = -ball->vx;
+			if (rPaddle->prevY != rPaddle->rect.y)
+				ball->vy += rPaddle->prevY - rPaddle->rect.y;
+		} else {
+			/* the left player scored a point */
+			lPlayer->points++;
+			resetAndServe(ball, RIGHT);
+		}
+	}
+	if (ball->rect.y <= court->upperWall.y+court->upperWall.h
+			|| ball->rect.y+ball->rect.h >= court->lowerWall.y) {
+		/* the ball has collided with a wall */
+		ball->vy = -ball->vy;
+	}
+
+	/* make sure the ball isn't clipped by overlapping paddles */
+	ball->rect.w = ball->rect.h = B_SIZE;
+
+	/* actually move the ball */
+	ball->rect.x += ball->vx;
+	ball->rect.y += ball->vy;
+}
+
+void resetAndServe(Ball *ball, Direction direction)
+{
+	ball->rect.x = (C_X+C_WIDTH)/2 - B_SIZE/2;
+	ball->rect.y = (C_Y+C_HEIGHT)/2 - B_SIZE/2;
+	ball->vy = 0;
+	if (direction == LEFT)
+		ball->vx = -B_VX;
+	else if (direction == RIGHT)
+		ball->vx = B_VX;
 }
 
 inline void die()
--- spong.h
+++ spong.h
@@ -14,15 +14,18 @@
 #define W_HEIGHT 480		/* the height of the main window */
 #define W_COLOR_DEPTH 32	/* the number of bits per pixel */
 
+#define C_X 0		/* the leftmost coordinate of the court */
+#define C_Y 0		/* the upper coordinate of the court */
 #define C_WIDTH 800	/* the width of the playing court */
 #define C_HEIGHT 480	/* the height of the playing court */
 
 #define P_WIDTH 20	/* the width of a paddle */
-#define P_HEIGHT 40	/* the height of a paddle */
+#define P_HEIGHT 60	/* the height of a paddle */
 #define P_STEP 10	/* the number of pixels a paddle moves
 			   in the duration of one frame */
 
 #define B_SIZE 20	/* the width and height of the ball */
+#define B_VX 10		/* the horizontal speed of the ball */
 
 #define FPS 30				/* frames per second */
 #define FRAME_DURATION ((int) 1000/FPS)	/* the duration of a frame in ms */
@@ -42,6 +45,8 @@ typedef struct Ball {
 
 typedef struct Paddle {
 	SDL_Rect rect;	/* the size and position of the paddle */
+	Uint32 prevY;	/* the paddle's y-value from the previous frame;
+			   used to determine whether the paddle is moving */
 	Uint32 color;	/* the color of the paddle */
 } Paddle;
 
@@ -52,7 +57,9 @@ typedef struct Player {
 
 typedef enum Direction {
 	UP,
-	DOWN
+	DOWN,
+	LEFT,
+	RIGHT
 } Direction;
 
 /* clear the screen to its background color */
@@ -64,6 +71,9 @@ Court *makeCourt(SDL_Surface *screen);
 /* initialize a player */
 Player *makePlayer(SDL_Surface *screen);
 
+/* initialize and draw the ball */
+Ball *makeBall(SDL_Surface *screen);
+
 /* read input from the keyboard and set the appropriate flags */
 void readPlayerInput(bool *running,
 		bool *player1ShouldMoveUp, bool *player1ShouldMoveDown,
@@ -71,6 +81,12 @@ void readPlayerInput(bool *running,
 
 /* move a player's paddle */
 void movePaddle(Court *court, Paddle *paddle, Direction direction);
+
+/* move the ball, checking for collisions and points scored */
+void moveBall(Court *court, Ball *ball, Player *lPlayer, Player *rPlayer);
+
+/* serve the ball after a point is scored */
+void resetAndServe(Ball *ball, Direction direction);
 
 /* print any errors and exit with a non-zero return status */
 void die();
